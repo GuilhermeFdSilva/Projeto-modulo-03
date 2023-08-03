54644cd845a09a11542b63875ae2f7ae67123864
Guilherme <francaguilherme27@gmail.com>
2023-08-02 22:44:51 -0300

    Barra de pesquisa

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,15 @@
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { Component } from '@angular/core';
+// Material UI
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss'],
   standalone: true,
-  imports: [MatToolbarModule]
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule]
 })
 export class NavBarComponent {
 

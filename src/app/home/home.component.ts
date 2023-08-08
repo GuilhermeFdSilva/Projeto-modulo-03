8d4b534d789dedca553615105caf73706054c9d4
Guilherme <francaguilherme27@gmail.com>
2023-08-07 22:41:26 -0300

    Criação dos cards

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,17 +3,17 @@ import { AnimeService } from './../../assets/data/animes.service';
 import { Anime } from './../../assets/data/animes.service';
 
 import { Component } from '@angular/core';
-import { NgFor, NgIf, NgClass } from '@angular/common';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatGridListModule} from '@angular/material/grid-list';
+import { NgFor, NgIf } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
   standalone: true,
-  imports: [NgFor, NgIf, NgClass, MatButtonModule, MatCardModule, MatGridListModule]
+  imports: [NgFor, NgIf, MatButtonModule, MatIconModule]
 })
 export class HomeComponent {
   animes: Array<Anime>;
